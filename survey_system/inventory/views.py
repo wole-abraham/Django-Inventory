ac767680c6af52d189910bb62541967efe03e72a
wole Abraham <wole1702@gmail.com>
2025-02-17 01:26:02 -0800

    added more users

--- survey_system/inventory/views.py
+++ survey_system/inventory/views.py
@@ -24,11 +24,15 @@ def filter_equipment(request):
 def request_equipment(request):
     if request.method == 'POST':
         surveyor = request.POST.get('surveyor_res')
+        section = request.POST.get('section')
+        project = request.POST.get('project')
         
         equipment = EquipmentsInSurvey.objects.filter(id=request.POST.get('id')).first()
 
         # Update equipment status and requested_by field
         equipment.status = 'In Field'
+        equipment.section = section
+        equipment.project = project
         equipment.surveyor_responsible = surveyor
         equipment.save()
         return redirect('profile')
@@ -55,16 +59,13 @@ def dashboard_view(request):
 def return_equipment(request):
     if request.method == 'POST':
         surveyor = request.POST.get('surveyor_res')
-        section = request.POST.get('seection')
-        project = request.POST.get('project')
+       
         print(request.POST.get('id'))
         equipment = EquipmentsInSurvey.objects.filter(id=request.POST.get('id')).first()
 
         # Update equipment status and requested_by field
         equipment.status = 'In Store'
         equipment.surveyor_responsible = surveyor
-        equipment.section = section
-        equipment.project = project
         equipment.save()
 
         messages.success(request, f'{equipment.name} has been returned successfully.')
